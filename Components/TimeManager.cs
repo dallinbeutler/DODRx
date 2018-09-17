7f8b9dd6f9d141c68114001e4474426299db27e8
Dallin Beutler <BeutlerD@corp.lcl>
2018-09-17 09:14:40 -0600

    got dilation to a more comfortable spot

--- Components/TimeManager.cs
+++ Components/TimeManager.cs
@@ -10,11 +10,48 @@ using System.Threading;
 
 namespace Components
 {
-   //struct TimedObservable
-   //{
-   //   long EntityID;
-   //   TimeSpan Timeleft;
-   //}
+   class DilationSystem:DataStream<long,Tuple<bool,double>>
+   {
+      double GlobalDilation;
+      Stopwatch stopwatch;
+
+      public DilationSystem()
+      {
+         stopwatch.Start();
+      }
+
+      public IObservable<long> DilatedTimer(long Entity, TimeSpan duration)
+      {
+
+         return Observable.Create((IObserver<long> observer) =>
+         {
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            var info = this[Entity];
+            var LastTick = stopwatch.Elapsed;
+            var elapsed = new TimeSpan();
+            var ticker = Observable.Interval(new TimeSpan(0, 0, 0, 0, 1 / 30000))
+            .Subscribe(x =>
+            {
+               if (!info.Item1)
+               {
+                  var timeBetween = LastTick - stopwatch.Elapsed;
+                  elapsed += TimeSpan.FromTicks((long)(timeBetween.Ticks * info.Item2));
+                  if (elapsed > duration)
+                  {
+
+                     observer.OnCompleted();
+                  }
+               }
+               LastTick = stopwatch.Elapsed;
+
+            });
+
+            return Disposable.Empty;
+         });
+      }
+
+   }
 
    class TimeManager
    {
@@ -28,50 +65,15 @@ namespace Components
       //List<Tuple<long, TimeSpan>> timers;
       //TimeSpan FPS = new TimeSpan(0, 0, 0, 0, 1 / 60000);
 
-      float GlobalDilation { get; set; } = 1.0f;
+      //float GlobalDilation { get; set; } = 1.0f;
 
-      public bool Paused
-      {
-         get
-         {
-            return Timer.IsRunning;
-         }
-         set
-         {
-            if (value)
-               Timer.Start();
-            else
-               Timer.Stop();
-         }
-      }
-      public double Dilation = 1.0f;
-      Stopwatch Timer;
+      //public bool Paused { get; set; }
 
-      public IObservable<long> DilatedTimer(TimeSpan duration)
-      {
-         return Observable.Create((IObserver<long> observer) =>
-         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            TimeSpan elapsed = new TimeSpan();
-            var ticker = Observable.Interval(new TimeSpan(0, 0, 0, 0, 1 / 30000))
-            .Subscribe(x =>
-                  {
-                     if (!Paused)
-                     {
-                        elapsed += new TimeSpan((long)(stopwatch.Elapsed.Ticks * Dilation));
-                        if (elapsed > duration)
-                        {
-                           observer.OnCompleted();
-                        }
-                     }
-                     stopwatch.Restart();
-                  });
+      //public double Dilation = 1.0f;
+      //Stopwatch Timer;
 
+      
 
-            return Disposable.Empty;
-         });
-      }
 
    }
 
