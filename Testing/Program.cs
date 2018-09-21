23f042dacffbdece93e32e0e7edbcc86e8f1b4ea
dallinbeutler <dallinbeutler@gmail.com>
2018-09-20 21:52:18 -0600

    Converted to a much more loosely coupled Approach. we'll see!

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -7,7 +7,8 @@ namespace Testing
       static void Main(string[] args)
       {
          Console.WriteLine("Start of Program!");
-         Game game = new Game();
+         //Game game = new Game();
+         Game2 game = new Game2();
          Console.WriteLine("Program ended.");
          Console.ReadKey();
       }
