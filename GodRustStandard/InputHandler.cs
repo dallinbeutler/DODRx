4104626492dca5d56e4d260eb7abfdfcd4e21c4c
dallinbeutler <dallinbeutler@gmail.com>
2018-09-25 22:57:25 -0600

    fixed some errors. added Nez. updated to 3.7

---
+++ GodRustStandard/InputHandler.cs
@@ -0,0 +1,47 @@
+﻿//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Input;
+//using MonoGame.Extended.Input;
+//using MonoGame.Extended.Input.InputListeners;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.Concurrent;
+//using System.Collections.Specialized;
+//using System.Reactive.Linq;
+//using System.Collections.ObjectModel;
+//using DOD;
+
+//namespace GodRustShared
+//{
+//   public class InputHandler
+//   {
+//      public DataStream<Buttons, bool> ButtonStates = new DataStream<Buttons, bool>() ;
+//      public InputHandler()
+//      {
+//         gplistener = new GamePadListener();
+//         gplistener.ButtonDown += Gplistener_ButtonDown;
+//         gplistener.ButtonUp += Gplistener_ButtonUp;
+
+//         ButtonStates[Buttons.LeftThumbstickLeft] = false;
+//         ButtonStates[Buttons.LeftThumbstickRight] = false;
+//         ButtonStates[Buttons.LeftThumbstickUp] = false;
+//         ButtonStates[Buttons.LeftThumbstickDown] = false;
+
+//      }
+
+//      private void Gplistener_ButtonUp(object sender, GamePadEventArgs e)
+//      {
+//         ButtonStates[e.Button] = false;
+//      }
+
+//      private void Gplistener_ButtonDown(object sender, GamePadEventArgs e)
+//      {
+//         ButtonStates[e.Button] = true;
+//      }
+//      GamePadListener gplistener { get; set; }
+//      public void Update(GameTime gameTime)
+//      {
+//         gplistener.Update(gameTime);
+//      }
+      
+//   }
+//}
