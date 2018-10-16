75ca4ad6b7c3ef649937f3b2954b65cbb97cb2e0
Dallin Beutler <DallinBeutler@Github.com>
2018-10-16 16:02:44 -0600

    Node ideas

---
+++ DOD/RxNode.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+using System.Linq;
+
+namespace DOD
+{
+   class RxNode : INotifyPropertyChanged, IDisposable
+   {
+      private RxNode parent;
+
+      RxNode Parent { get => parent;
+         set
+         {
+            if (parent == value) return;
+            if (parent != null)
+            {
+               parent.PropertyChanged -= Parent_PropertyChanged;
+            }
+            parent = value;
+            parent.PropertyChanged += Parent_PropertyChanged;
+            PropertyChanged.Invoke(this,new PropertyChangedEventArgs("Parent"));
+         }
+      }
+
+      public IEnumerable<RxNode> Siblings(IEnumerable<RxNode> tree)
+      {
+         return tree.Where(x => x.Parent == Parent);
+      }
+      public IEnumerable<RxNode> Children(IEnumerable<RxNode> tree)
+      {
+         return tree.Where(x => x.Parent == this);
+      }
+      public int TreeDepth()
+      {
+         int depth = 0;
+         var p = parent;
+         while (p != null)
+         {
+            depth++;
+            p = p.Parent;
+         }
+         return depth;
+      }
+
+      protected void Parent_PropertyChanged(object sender, PropertyChangedEventArgs e)
+      {
+      }
+      //public event GetChildren GetChildrenEvent;
+      //public delegate RxNode GetChildren();
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      public event EventHandler Disposing;
+      public void Dispose()
+      {
+         Disposing.Invoke(this, new EventArgs());
+      }
+
+      static IEnumerable<T> CallBackToEnumerable<T>(Action<Action<T>> functionReceivingCallback)
+      {
+         return Observable.Create<T>(o =>
+         {
+            // Schedule this onto another thread, otherwise it will block:
+            System.Reactive.Concurrency.Scheduler.Schedule(NewThreadScheduler.Default, () =>
+            {
+               functionReceivingCallback(o.OnNext);
+               o.OnCompleted();
+            });
+            return () => { };
+         }).ToEnumerable();
+      }
+   }
+
+   interface IUpdate
+   {
+      void Update(float delta);
+   }
+   interface IRender
+   {
+      int ZOrder { get; set; }
+      void Render(float delta);
+   }
+
+   interface IDil : IUpdate
+   {
+      float Dilation { get; set; }
+      bool AffectedByParent { get; set; }
+   }
+}
---
+++ Testing/Testing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DynamicData;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using ReactiveUI;
+
+
+namespace Testing
+{
+   class Testing
+   {
+      //Node<>
+      SourceCache<KeyValuePair<long, TimeSpan>, long> source = new SourceCache<KeyValuePair<long,TimeSpan>, long>(x=> { return 1 ; });
+      public Testing()
+      {
+         //source.AsObservableCache().ObservableForProperty(x=>x.)
+         source.Lookup(2);
+         //source.Watch(0).Subscribe(x => { x.c = 2; });
+         //var c = source.Connect().Filter(x=>x.Value.Days == 3).Sort(SortExpre)
+         //c.ToObservableChangeSet(expireAfter: x=>new TimeSpan(0,1,0)).Subscribe(x=>x.i);
+
+      }
+      
+   }
+}
